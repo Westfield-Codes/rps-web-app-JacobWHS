37c64c084c599f196c31e3895cd44e75a5da025d
lin., frank <jacob127871@student-schoolsofwestfield.org>
2024-12-12 16:06:34 +0000

    22 tasks

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /* Function getRounds
  * pulls rounds from the input box on the home page and sends to setRounds
  * @param = none
@@ -16,14 +17,28 @@ function getRounds(){
  * @return = none
  */
 function setRounds(rounds){
-    if (rounds % 2 == 0) {
-        alert("must be odd");
+    if (rounds % 2 == 0 || isNaN(rounds)) {
+        //alert("must be odd");
+        document.getElementById("rounds").value = "";
+        if (rounds % 2 == 0){
+        document.getElementById("rounds").placeholder = "Your input must be odd.";
+        }
+        else{
+            document.getElementById("rounds").placeholder = "Your input must be a number.";
+        }
     }
     else {
-        localStorage.setItem("rounds",rounds);
-        localStorage.setItem("round",1);
+        let score = [0, 0];
+        localStorage.setItem("score", JSON.stringify(score));
+        localStorage.setItem("rounds", rounds);
+        localStorage.setItem("round", 1);
         window.location.href = "chooser.html";
     }
+}
+
+function endGame(){
+    if (score[0] > score[1]) localStorage.setItem("winner", "You");
+    else localStorage.setItem("winner", "I");
 }
 
 /* Function showRound
@@ -35,51 +50,62 @@ function setRounds(rounds){
 function showRound(){
     let round = localStorage.getItem("round");
     let rounds = localStorage.getItem("rounds");
+    let score = JSON.parse(localStorage.getItem("score"));
     if (round > rounds) {
         window.location.href = "gameover.html";
     }
     let statsBox = document.getElementById("statsBox");
     let message = "Round " + round + " of " + rounds;
     statsBox.innerHTML = message;
+    let scoreBox = document.getElementById("scoreBox");
+    scoreBox.innerHTML = "Score: " + score.toString();
 }
 
 /* Function cpuTurn
  * Called by buttons in chooser with userturn u as argument.
  * Generates a computer turn c and sends u and c to findWinner
  * @param = u
- * @return = u,c
+ * @return = u, c
  */
 function cpuTurn(u){
     let moves = ["r","p","s"];
     let choice = Math.floor(Math.random()*3);
     let c = moves[choice];
-    findWinner(u,c);
+    findWinner(u, c);
 }
 
 /* Function findWinner
  * Checks if u & c are equal.  If not, finds winner and updates showRound, 
  * Incrementing round and saving to local storage.
- * @param = u,c
+ * @param = u, c
  * @localStorage <=> round++
  * @return = none
  */
-function findWinner(u,c){
+function findWinner(u, c){
     if (u == c){
-        alert("We both picked " + u);
+        //alert("We both picked " + u);
+        document.getElementById("result").innerHTML = "We both chose " + u + ".";
     }
     else {
         let winner = " ";
-        let winArray=[["r","p","I"],["r","s","you"],["p","s","I"],["p","r","you"],["s","r","I"],["s","p","you"]];
+        let winArray = [["r","p","I"],["r","s","You"],["p","s","I"],["p","r","You"],["s","r","I"],["s","p","You"]];
         for (let i = 0; i< winArray.length; i++){
             if (winArray[i][0] == u && winArray[i][1]==c){
-                winner= winArray[i][2];
+                winner = winArray[i][2];
 
             }
         }
-        alert("You choose " + u + " and I choose " + c + " " + winner + " win!");
+        // alert("You choose " + u + " and I choose " + c + " " + winner + " win!");
+        document.getElementById("result").innerHTML = "You choose " + u + " and I choose " + c + " " + winner + " win!";
         let round = localStorage.getItem("round");
+        const players = ["You", "I"];
         round++;
-        localStorage.setItem("round",round);
+        let win = players.indexOf(winner);
+        let score = JSON.parse(localStorage.getItem("score"));
+        score[win]++;
+        localStorage.setItem("score", JSON.stringify(score));
+        localStorage.setItem("round", round);
+        localStorage.setItem("winner", winner);
         showRound();
     }
 }
